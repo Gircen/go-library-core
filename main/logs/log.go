78666f398a651822c6d7c23086da340b531399c9
antonkolesnikov <girrcen666@icloud.com>
2025-07-29 11:15:17 +0300

    update log

--- main/logs/log.go
+++ main/logs/log.go
@@ -1,7 +1,8 @@
 package logs
 
 import (
-	"github.com/Gircen/go-library-api/main/config"
+	logEnum "github.com/Gircen/go-library-api/main/log"
+	"github.com/Gircen/go-library-core/main/conf"
 	"io"
 	"log"
 	"os"
@@ -9,17 +10,35 @@ import (
 
 var Logger *log.Logger
 
-func Log(config *config.Config) *log.Logger {
+func Warn(v ...any) {
+	if logEnum.IsLog(conf.Conf.Log.Level, logEnum.WARN) {
+		Logger.Println(logEnum.WARN.String(), v)
+	}
+}
+
+func Error(v ...any) {
+	Logger.Println(logEnum.ERROR.String(), v)
+}
+
+func Info(v ...any) {
+	Logger.Println(logEnum.INFO.String(), v)
+}
+
+func Debug(v ...any) {
+	Logger.Println(logEnum.DEBUG.String(), v)
+}
+
+func Log() *log.Logger {
 
 	err := os.MkdirAll("log/", os.ModePerm)
 	if err != nil {
 		return nil
 	}
-	f, err := os.OpenFile("log/"+config.App.Name+".log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
+	f, err := os.OpenFile("log/"+conf.Conf.App.Name+".log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var logger = log.New(f, config.App.Name, log.LstdFlags)
+	var logger = log.New(f, conf.Conf.App.Name, log.LstdFlags|log.Llongfile)
 	mw := io.MultiWriter(os.Stdout, f)
 	logger.SetOutput(mw)
 	return logger
